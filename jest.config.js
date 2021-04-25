f3f4b612014c0f0c2bdcf845256997cf448f498b
kwonmory <kwonmory@gmail.com>
2021-04-25 13:56:20 +0900

    update: 테스트를 위한 추가적인 설정

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,26 @@
 module.exports = {
-  verbose: true,
-  setupFilesAfterEnv: [
-    'given2/setup',
-    'jest-plugin-context/setup',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
+	setupFilesAfterEnv: ['given2/setup', 'jest-plugin-context/setup'],
+
+	moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+	transform: {
+		'^.+\\.(js|jsx)?$': 'babel-jest',
+		'^.+\\.(ts|tsx)?$': 'ts-jest',
+	},
+	testEnvironment: 'jsdom',
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/$1',
+	},
+	testMatch: [
+		'<rootDir>/**/*.test.(js|jsx|ts|tsx)',
+		'<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+	],
+	transformIgnorePatterns: ['<rootDir>/node_modules/'],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 };
